2e3824e8847ae2d8f2cb9a2e3052d8ffe41369bd
vlcoscar8 <vlcoscar8@gmail.com>
2023-07-21 14:37:12 +0200

    Ejercicio 2: writeFile

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-// Ejercicio 2
-async function writeFile (filePath, data, callback) {
+const fs = require('node:fs/promises');
+const path = require('node:path');
 
+// Ejercicio 2
+async function writeFile(filePath, data, callback) {
+    try {
+        const dir = path.dirname(filePath);
+        await fs.mkdir(dir, { recursive: true });
+    } catch (error) {
+        console.log('Error creando directorio', error);
+        process.exit(1);
+    }
+
+    try {
+        await fs.writeFile(filePath, data);
+    } catch (error) {
+        console.log(`Error creando fichero en la ruta: ${filePath}`, error);
+        process.exit(1);
+    }
+
+    callback();
 }
 
 // Ejercicio 3
-async function readFileAndCount (word, callback) {
-
-}
+async function readFileAndCount(word, callback) {}
 
 module.exports = {
-  writeFile,
-  readFileAndCount
-}
+    writeFile,
+    readFileAndCount,
+};
